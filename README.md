4304524a40a3ef1ff0646a91beeb1fddac60d34c
felipe-jaque-dev <felipealexandrej@gmail.com>
2023-06-05 21:51:45 -0300

    Menu Hamburger/Carrossel implementados

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Criar um site de e-commerce de suplementos alimentares com páginas para cada ca
 - Um cabeçalho com moderno e intuitivo com design responsivo
 * Uma página inicial com os produtos em destaque, o cabeçalho tera um menu de produtos que leva às páginas com os produtos mais especificos 
 * Criar paginas para produto com informações mais detalhadas sobre o produto, incluindo descrição, benefícios, composição, modo de uso e informações nutricionais.
-* Inicialmente o projeto não contara com as funcionalidades "Entrar" e "Cadastre-se", que se encontram no layout.
  
  
  📝**Linguagens utilizadas**
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+//altera o display do form cadastro
+
 function singup() {
     var formulario = document.getElementById("janela-cadastro");
     formulario.style.display = "block";
@@ -8,11 +10,11 @@ function closesingup() {
     formulario.style.display = "none";
   }
   
-  // Adicione um event listener para o botão de fechar
+  //evento para botão fechar
   var closeButton = document.getElementById("close-button");
   closeButton.addEventListener("click", closesingup);
 
-  //evento de clique "login"
+  //altera o display do form login
 
   function toggleLogin() {
   var loginForm = document.querySelector('.login-container');
@@ -25,4 +27,64 @@ function closesingup() {
   }
 }
 
+//LEMBRAR DE FAZER O BOTÃO DE FECHAR xD
+
+// MENU HAMBURGER
+const brnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+
+const nav = document.getElementById('nav');
+nav.classList.toggle('active');
+}
+
+brnMobile.addEventListener('click', toggleMenu);
+brnMobile.addEventListener('touchstart', toggleMenu);
+
+
+// EVENTO CLIQUE PARA CAIXAS PAG INI
+
+const boxes = document.getElementsByClassName('square-box');
+
+for (let i = 0; i < boxes.length; i++) {
+  const box = boxes[i];
+  const boxTitle = box.querySelector('.box-tittle');
+  const boxText = box.querySelector('.box-text');
+
+  let isBoxTextVisible = false;
+
+  box.addEventListener('click', function() {
+    if (isBoxTextVisible) {
+      boxText.style.display = 'none';
+      boxTitle.style.display = 'block';
+      isBoxTextVisible = false;
+    } else {
+      boxTitle.style.display = 'none';
+      boxText.style.display = 'block';
+      isBoxTextVisible = true;
+    }
+  });
+}
+
+      // CARROSSEL Em manutenção xD
+
+      // Seleciona os elementos
+const carrossel = document.querySelector('.carrossel');
+const logotiposContainer = document.querySelector('.logotipos');
+
+
+
+// Calcula a largura total dos logotipos
+const logotiposWidth = logotiposArray.reduce((total, logotipo) => total + logotipo.offsetWidth, 0);
+
+// Verifica se há espaço suficiente para exibir os logotipos
+function verificarEspaco() {
+  const carrosselWidth = carrossel.offsetWidth;
+  
+  if (carrosselWidth >= logotiposWidth) {
+    logotiposContainer.style.width = '100%';
+  } else {
+    logotiposContainer.style.width = `${logotiposWidth}px`;
+  }
+}
 
